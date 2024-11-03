fd1941ad5476435a5432d71bcc21912eb4a1a8c7
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-11-03 23:01:52 +0500

    Figma  Thursday 31/Oct/2k24

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "@/components/Header";
+import Image from "next/image";
+
+function Home() {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className="flex h-[80%] justify-center items-center">
+        <div className="max-w-[548px] flex flex-col justify-center items-start mx-4">
+          <h1 className="text-[44px] font-bold">Welcome To Our Website</h1>
+          <p className="text-[32px] leading-[38.73px] mb-4 font-inter text-gray-700">
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard.
+</p>
+
+          <button className="bg-black text-white w-[237px] h-[66px] mt-4 text-[28px]">Contact Us</button>
+        </div>
+        <div className="max-w-[502px] mx-4">
+          <Image 
+            src="/figma_image.svg" 
+            alt="Figma"
+            width={502} 
+            height={465} 
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="w-full h-[88px] bg-gray-300 flex justify-center items-center">
+    
+    </div>
+  );
+}
+
+export default Header;
